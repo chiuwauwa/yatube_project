afe9afe29042017dd5b890de212a8829ce32b4d1
Никита Павздерин <nipavzderin@gmail.com>
2024-02-08 16:43:49 +0500

    3 спринт, полностью рабочий

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,30 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+from yatube.settings import QTY_POSTS_PAGE
 
 # Create your views here.
-def index(request):
-    return HttpResponse('Самая главная страница!'
-                        ' Главнее не будет, но будут другие')
 
-def group_posts(request, sl):
-    return HttpResponse(f'А это невьебенный пост от нашего блогера {sl}')
+
+def index(request):
+    template = 'posts/index.html/'
+    posts = posts = Post.objects.all()[:QTY_POSTS_PAGE]
+    title = 'Последние обновления на сайте'
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    template = 'posts/group_list.html/'
+    posts = group.posts.all()[:QTY_POSTS_PAGE]
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
+#    return HttpResponse(f'А это невьебенный пост от нашего блогера {sl}')
